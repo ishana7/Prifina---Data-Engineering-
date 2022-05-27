eedaecaafe42125c89e251772eaa0ce87ce6fb5a
Ishana <45902415+ishana7@users.noreply.github.com>
2022-05-27 13:27:34 -0400

    Add files via upload

---
+++ Data Engineering Test Task/README.txt
@@ -0,0 +1,25 @@
+# Prifina Data Engineering Test Task
+
+
+# Contents of the p5 folder
+
+* index.html file
+* script.js file
+* style.css file 
+
+
+## script.js
+
+This file contains the javascript code for the data visualization.
+
+## style.css
+
+This file is a template for a CSS stylesheet. 
+
+## index.html
+
+index.html is a template for an HTML file. This index.html file imports the anychart javascript library that helps to embed interactive ang good looking charts and dashboards to any webpage. 
+
+## README.txt
+
+This README file formatted with Markdown :)
---
+++ Data Engineering Test Task/script.js
@@ -0,0 +1,342 @@
+var fitbit_data =[
+    {
+      "Date": "01-05-2022",
+      "Calories_Burned": 1317,
+      "Steps": 3539,
+      "Distance": 2.41,
+      "Floors": 0,
+      "Minutes_Sedentary": 1395,
+      "Minutes_Lightly_Active": 85,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 175,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "02-05-2022",
+      "Calories_Burned": 1317,
+      "Steps": 1906,
+      "Distance": 1.93,
+      "Floors": 0,
+      "Minutes_Sedentary": 1395,
+      "Minutes_Lightly_Active": 120,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 213,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "03-05-2022",
+      "Calories_Burned": 1317,
+      "Steps": 1906,
+      "Distance": 1.93,
+      "Floors": 0,
+      "Minutes_Sedentary": 1440,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 150,
+      "Activity": "Run"
+    },
+    {
+      "Date": "04-05-2022",
+      "Calories_Burned": 1399,
+      "Steps": 3539,
+      "Distance": 2.41,
+      "Floors": 0,
+      "Minutes_Sedentary": 1350,
+      "Minutes_Lightly_Active": 90,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 165,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "05-05-2022",
+      "Calories_Burned": 2058,
+      "Steps": 0,
+      "Distance": 0,
+      "Floors": 0,
+      "Minutes_Sedentary": 1170,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 270,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 988,
+      "Activity": "Bike"
+    },
+    {
+      "Date": "06-05-2022",
+      "Calories_Burned": 1357,
+      "Steps": 2123,
+      "Distance": 1.45,
+      "Floors": 0,
+      "Minutes_Sedentary": 1395,
+      "Minutes_Lightly_Active": 45,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 82,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "07-05-2022",
+      "Calories_Burned": 2050,
+      "Steps": 2700,
+      "Distance": 2.74,
+      "Floors": 0,
+      "Minutes_Sedentary": 1300,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 45,
+      "Minutes_Very_Active": 95,
+      "Activity_Calories": 861,
+      "Activity": "Run"
+    },
+    {
+      "Date": "08-05-2022",
+      "Calories_Burned": 1442,
+      "Steps": 0,
+      "Distance": 0,
+      "Floors": 0,
+      "Minutes_Sedentary": 1394,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 46,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 167,
+      "Activity": "Swim"
+    },
+    {
+      "Date": "09-05-2022",
+      "Calories_Burned": 1606,
+      "Steps": 0,
+      "Distance": 0,
+      "Floors": 0,
+      "Minutes_Sedentary": 1335,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 105,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 385,
+      "Activity": "Bike"
+    },
+    {
+      "Date": "10-05-2022",
+      "Calories_Burned": 1605,
+      "Steps": 1906,
+      "Distance": 1.93,
+      "Floors": 0,
+      "Minutes_Sedentary": 1395,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 45,
+      "Activity_Caloriess": 330,
+      "Activity": "Swim"
+    },
+    {
+      "Date": "11-05-2022",
+      "Calories_Burned": 1400,
+      "Steps": 4719,
+      "Distance": 3.22,
+      "Floors": 0,
+      "Minutes_Sedentary": 1380,
+      "Minutes_Lightly_Active": 60,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 138,
+      "Activity": "Run"
+    },
+    {
+      "Date": "12-05-2022",
+      "Calories_Burned": 1728,
+      "Steps": 0,
+      "Distance": 0,
+      "Floors": 0,
+      "Minutes_Sedentary": 1290,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 150,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 549,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "13-05-2022",
+      "Calories_Burned": 1358,
+      "Steps": 2831,
+      "Distance": 1.93,
+      "Floors": 0,
+      "Minutes_Sedentary": 1395,
+      "Minutes_Lightly_Active": 45,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 83,
+      "Activity": "Swim"
+    },
+    {
+      "Date": "14-05-2022",
+      "Calories_Burned": 1399,
+      "Steps": 3067,
+      "Distance": 2.09,
+      "Floors": 0,
+      "Minutes_Sedentary": 1350,
+      "Minutes_Lightly_Active": 90,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 165,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "15-05-2022",
+      "Calories_Burned": 1414,
+      "Steps": 7079,
+      "Distance": 4.83,
+      "Floors": 0,
+      "Minutes_Sedentary": 1335,
+      "Minutes_Lightly_Active": 105,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 193,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "16-05-2022",
+      "Calories_Burned": 1728,
+      "Steps": 0,
+      "Distance": 0,
+      "Floors": 0,
+      "Minutes_Sedentary": 1290,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 150,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 549,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "17-05-2022",
+      "Calories_Burned": 1394,
+      "Steps": 4247,
+      "Distance": 2.9,
+      "Floors": 0,
+      "Minutes_Sedentary": 1410,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 30,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 105,
+      "Activity": "Bike"
+    },
+    {
+      "Date": "18-05-2022",
+      "Calories_Burned": 1409,
+      "Steps": 4719,
+      "Distance": 3.22,
+      "Floors": 0,
+      "Minutes_Sedentary": 1389,
+      "Minutes_Lightly_Active": 51,
+      "Minutes_Fairly_Active": 0,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 139,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "19-05-2022",
+      "Calories_Burned": 2073,
+      "Steps": 9438,
+      "Distance": 6.44,
+      "Floors": 0,
+      "Minutes_Sedentary": 1065,
+      "Minutes_Lightly_Active": 150,
+      "Minutes_Fairly_Active": 225,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 1099,
+      "Activity": "Walk"
+    },
+    {
+      "Date": "20-05-2022",
+      "Calories_Burned": 1807,
+      "Steps": 3539,
+      "Distance": 2.41,
+      "Floors": 0,
+      "Minutes_Sedentary": 1205,
+      "Minutes_Lightly_Active": 85,
+      "Minutes_Fairly_Active": 150,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 705,
+      "Activity": "Bike"
+    },
+    {
+      "Date": "21-05-2022",
+      "Calories_Burned": 944,
+      "Steps": 1903,
+      "Distance": 1.4,
+      "Floors": 0,
+      "Minutes_Sedentary": 1033,
+      "Minutes_Lightly_Active": 0,
+      "Minutes_Fairly_Active": 35,
+      "Minutes_Very_Active": 0,
+      "Activity_Calories": 390,
+      "Activity": "Walk"
+    }
+   ]
+   
+   var data = document.querySelector('.fitbit-activity')
+   
+   
+   
+   function visualisation() {
+     var data = {
+          header: ["Date", "Calories_Burned"],
+       rows: [
+           ["01-05-2022", 1317],
+           ["02-05-2022", 1317],
+           ["03-05-2022", 1317],
+           ["04-05-2022", 1399],
+           ["05-05-2022", 2058],
+           ["06-05-2022", 1357],
+           ["07-05-2022", 2050],
+           ["08-05-2022", 1442],
+           ["09-05-2022", 1606],
+           ["10-05-2022", 1605],
+           ["11-05-2022", 1400],
+           ["12-05-2022", 1728],
+           ["13-05-2022", 1358],
+           ["14-05-2022", 1399],
+           ["15-05-2022", 1414],
+           ["16-05-2022", 1728],
+           ["17-05-2022", 1394],
+           ["18-05-2022", 1409],
+           ["19-05-2022", 2073],
+           ["20-05-2022", 1807],
+           ["21-05-2022", 944]
+       ]};
+   var data1={header:["Activity","Count"],rows:[["Walk",0],["Bike",0],["Run",0],["Swim",0]]}
+   for(var i = 0; i < fitbit_data.length; i++){
+    if(fitbit_data[i].Activity== "Run"){ 
+     data1.rows[2][1]=   data1.rows[2][1]+fitbit_data[i].Calories_Burned;
+     } 
+     else if(fitbit_data[i].Activity== "Walk")
+     { 
+     data1.rows[0][1]=   data1.rows[0][1]+fitbit_data[i].Calories_Burned;
+     } else if(fitbit_data[i].Activity== "Bike")
+     { 
+     data1.rows[1][1]=   data1.rows[1][1]+fitbit_data[i].Calories_Burned;
+     } else{
+       data1.rows[3][1]=   data1.rows[3][1]+fitbit_data[i].Calories_Burned;
+     }
+   }
+       // create the chart
+      var chart = anychart.line();
+      var chart1 = anychart.pie();
+       // add data
+       chart.data(data);
+       chart1.data(data1);
+   
+       // set the chart title
+     chart.title("Number of Calories burned each day");
+     chart1.title("Activities that help burn more Calories");
+     // draw
+     chart.container("container");
+     chart1.container("container1")
+     chart.draw();
+     chart1.draw();
+   }
+   
+       // set the data
+     
+   
+   visualisation();
